cee50ad1a32290cdf7eefa887b1602d97fea4451
Princemashumu <113059693+Princemashumu@users.noreply.github.com>
2024-07-22 09:54:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+// import Login from './components/Login';
+// import Register from './components/Register';
+// import Home from './pages/Home';
+
+// const App = () => {
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+//   const handleLogin = () => {
+//     // Add login logic here
+//     setIsAuthenticated(true);
+//   };
+
+//   const handleSignup = () => {
+//     // Add signup logic here
+//     setIsAuthenticated(true);
+//   };
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/login">
+//           {isAuthenticated ? <Redirect to="/" /> : <Login handleLogin={handleLogin} />}
+//         </Route>
+//         <Route path="/Register">
+//           {isAuthenticated ? <Redirect to="/" /> : <Register handleSignup={handleSignup} />}
+//         </Route>
+//         <PrivateRoute path="Home/" isAuthenticated={isAuthenticated} component={Home} />
+//       </Switch>
+//     </Router>
+//   );
+// };
+
+// const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={(props) =>
+//       isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+//     }
+//   />
+
+// );
+// export default App
+
+// src/App.js
+
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './pages/Home';
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    // Add login logic here
+    setIsAuthenticated(true);
+  };
+
+  const handleSignup = () => {
+    // Add signup logic here
+    setIsAuthenticated(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
